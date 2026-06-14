250f4299bb8c9b0b08c0ce7145149743e82b4c0c
jonioss <giannisvar2015@gmail.com>
2026-06-14 11:48:39 +0300

    Roofline model added with plot and script to generate it.

---
+++ roofline_model/roofline_model.m
@@ -0,0 +1,25 @@
+%% set constants
+BW = 128;
+
+%% set intensity
+x = linspace(0, 5, 500);
+
+%% calculate performance
+y = BW*ones(size(x));
+mask = (x>=0) & (x<=1);
+y(mask) = BW .* x(mask);
+
+%% plot roofline model
+plot(x, y, 'LineWidth', 2);
+xlabel('Intensity (FLOPs/Byte)');
+ylabel('Performance (FLOPs/Cycle)');
+title('Roofline Model');
+grid on;
+hold on;
+
+%% plot achieved performance - intensity point
+achieved_I = 4.57;
+achieved_P = 25.19;
+plot(achieved_I, achieved_P, 'ro', 'MarkerSize', 8, 'DisplayName', 'Achieved');
+legend show;
+hold off;

c460a29be146343f0671d6180926443e57a8e64b
jonioss <giannisvar2015@gmail.com>
2026-07-09 16:14:47 +0300

    Achieved 945 cycles in cosim by increasing depth of A_streams and B_streams and rewinding inner loop of calculatepsum. Updated roofline model

--- source/constants.h
+++ source/constants.h
@@ -5,7 +5,8 @@
 
 #define NUM_OF_TILES 2
 
-typedef ap_fixed<32, 8> fm_t;
+//typedef ap_fixed<32, 8> fm_t;
+typedef float fm_t;
 
 void gemm1(fm_t A_DRAM[I][K], fm_t B_DRAM[K][J], fm_t C_DRAM[I][J]);
 void gemm2(const hls::vector<fm_t, 16> A_DRAM[I][K/16], const hls::vector<fm_t, 16> B_DRAM[K/16][J], hls::vector<fm_t, 16> C_DRAM[I][J/16]);
--- source/gemm.cpp
+++ source/gemm.cpp
@@ -13,15 +13,16 @@ void gemm2(const hls::vector<fm_t, 16> A_DRAM[I][K/16],const hls::vector<fm_t, 1
 	#pragma HLS INTERFACE m_axi offset=slave port=C_DRAM bundle=gmem2
 
 	fm_t A_BUF[I][K];
-	#pragma HLS ARRAY_PARTITION variable=A_BUF dim=1 type=complete
+	#pragma HLS bind_storage variable=A_BUF type=RAM_1WNR impl=BRAM
+	#pragma HLS ARRAY_PARTITION variable=A_BUF dim=1 type=block factor=NUM_OF_TILES
 	#pragma HLS ARRAY_PARTITION variable=A_BUF dim=2 type=complete
 	fm_t B_BUF[NUM_OF_TILES][K][J/NUM_OF_TILES];
-	#pragma HLS ARRAY_PARTITION variable=B_BUF dim=1 type=complete
+	#pragma HLS ARRAY_PARTITION variable=B_BUF dim=1 type=block factor=NUM_OF_TILES
 	#pragma HLS ARRAY_PARTITION variable=B_BUF dim=2 type=complete
 	#pragma HLS ARRAY_PARTITION variable=B_BUF dim=3 type=complete
 	fm_t C_BUF[NUM_OF_TILES][NUM_OF_TILES][I/NUM_OF_TILES][J/NUM_OF_TILES];
-	#pragma HLS ARRAY_PARTITION variable=C_BUF dim=1 type=complete
-	#pragma HLS ARRAY_PARTITION variable=C_BUF dim=2 type=complete
+	#pragma HLS ARRAY_PARTITION variable=C_BUF dim=1 type=block factor=NUM_OF_TILES
+	#pragma HLS ARRAY_PARTITION variable=C_BUF dim=2 type=block factor=NUM_OF_TILES
 	#pragma HLS ARRAY_PARTITION variable=C_BUF dim=4 type=complete
 	#pragma HLS bind_storage variable=C_BUF type=RAM_1WNR impl=BRAM
 	
@@ -30,14 +31,15 @@ void gemm2(const hls::vector<fm_t, 16> A_DRAM[I][K/16],const hls::vector<fm_t, 1
 	loop_for_tile_a:
 	for(int tileA = 0; tileA < NUM_OF_TILES; tileA++) {
 		#pragma HLS DATAFLOW
-		#pragma HLS stable variable=A_BUF
-    	#pragma HLS stable variable=B_BUF
+		#pragma HLS STABLE variable=A_BUF
+    	#pragma HLS STABLE variable=B_BUF
+		#pragma HLS STABLE variable=C_BUF
 		hls::stream<fm_t> A_streams[NUM_OF_TILES];
-		#pragma HLS STREAM variable=A_streams type=pipo depth=2
+		#pragma HLS STREAM variable=A_streams type=pipo depth=4
 		hls::stream<fm_t> C_streams[NUM_OF_TILES][J/NUM_OF_TILES];
 		#pragma HLS STREAM variable=C_streams type=pipo depth=2
 		hls::stream<fm_t> B_streams[NUM_OF_TILES][J/NUM_OF_TILES];
-	#pragma HLS stream variable=B_streams type=pipo depth=2
+		#pragma HLS stream variable=B_streams type=pipo depth=4
 		loader_A_tiles(A_BUF, A_streams, tileA);
 		loader_B_tiles(B_BUF, B_streams);
 		matMulTiles(A_streams, B_streams, C_streams);

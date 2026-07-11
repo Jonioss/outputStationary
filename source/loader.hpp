ed056d1e7bc004f9d83db3c347e446bca30b7e38
jonioss <giannisvar2015@gmail.com>
2026-07-11 12:30:44 +0300

    Done commenting

--- source/loader.hpp
+++ source/loader.hpp
@@ -1,6 +1,6 @@
 #include "constants.h"
 
-// Functions to stream A's values from the buffer to the matmultile functions
+// Functions to stream Buffer's values from the buffers to the matmultile functions
 void loader_A_tiles(const float A_BUF[I][K], hls::stream<float> A_streams[NUM_OF_TILES], const int tileA){
     #pragma HLS INLINE off // Pragma to turn off inlining for this function
 	for(int i = 0; i < I/NUM_OF_TILES; i++) {
@@ -15,7 +15,6 @@ void loader_A_tiles(const float A_BUF[I][K], hls::stream<float> A_streams[NUM_OF
 		}
 	}
 }
- 
 void loader_B_tile(const float B_TILE[K][J/NUM_OF_TILES], hls::stream<float> B_streams[J/NUM_OF_TILES]){
     #pragma HLS INLINE off // Pragma to turn off inlining for this function
 	for(int i = 0; i < I/NUM_OF_TILES; i++) {
@@ -29,7 +28,16 @@ void loader_B_tile(const float B_TILE[K][J/NUM_OF_TILES], hls::stream<float> B_s
 		}
 	}
 }
- 
+void loader_B_tiles(const float B_BUF[NUM_OF_TILES][K][J/NUM_OF_TILES], hls::stream<float> B_streams[NUM_OF_TILES][J/NUM_OF_TILES]){
+    #pragma HLS INLINE off
+    #pragma HLS DATAFLOW
+    for(int t=0; t<NUM_OF_TILES; t++){
+        #pragma HLS UNROLL
+        loader_B_tile(B_BUF[t], B_streams[t]); // Pass multiple tiles to multiple streams at the same time
+    }
+}
+
+// Functions to stream Buffer's values from the matmultile functions to the buffer
 void loader_C_tile(hls::stream<float> C_streams[J/NUM_OF_TILES], float C[I/NUM_OF_TILES][J/NUM_OF_TILES]){
     #pragma HLS INLINE off
     for(int i = 0; i < I/NUM_OF_TILES; i++) {
@@ -37,25 +45,15 @@ void loader_C_tile(hls::stream<float> C_streams[J/NUM_OF_TILES], float C[I/NUM_O
         #pragma HLS LOOP_FLATTEN
 		for(int j = 0; j < J/NUM_OF_TILES; j++) {
             #pragma HLS UNROLL
-            C[i][j] = C_streams[j].read();
+            C[i][j] = C_streams[j].read(); // Put C's values in a stream
 		}
 	}
 }
- 
-void loader_B_tiles(const float B_BUF[NUM_OF_TILES][K][J/NUM_OF_TILES], hls::stream<float> B_streams[NUM_OF_TILES][J/NUM_OF_TILES]){
-    #pragma HLS INLINE off
-    #pragma HLS DATAFLOW
-    for(int t=0; t<NUM_OF_TILES; t++){
-        #pragma HLS UNROLL
-        loader_B_tile(B_BUF[t], B_streams[t]);
-    }
-}
- 
 void loader_C_tiles(hls::stream<float> C_streams[NUM_OF_TILES][J/NUM_OF_TILES], float C_TILES[NUM_OF_TILES][I/NUM_OF_TILES][J/NUM_OF_TILES]){
     #pragma HLS INLINE off
     #pragma HLS DATAFLOW
     for(int t=0; t<NUM_OF_TILES; t++){
         #pragma HLS UNROLL
-        loader_C_tile(C_streams[t], C_TILES[t]);
+        loader_C_tile(C_streams[t], C_TILES[t]); // Pass multiple tiles to multiple streams at the same time
     }
 }
--- source/usefulFunctions.hpp
+++ source/usefulFunctions.hpp
@@ -4,14 +4,15 @@
 // -------------FOR TILING----------------
 // ---------------------------------------
 
+// Function to calculate partial sum
 void calculatepsum(hls::stream<float> &A_stream, hls::stream<float> B_streams[J/NUM_OF_TILES], float psum[J/NUM_OF_TILES]) {
 	#pragma HLS INLINE off
 	#pragma HLS DATAFLOW
 	for(int k = 0; k < K; k++) {
-		#pragma HLS PIPELINE II=1 rewind
+		#pragma HLS PIPELINE II=1 rewind // Allows the next iteration to start before the previous one ends
 		const float a_val = A_stream.read();
 		mat_mul_j:
-		for(int j = 0; j < J/NUM_OF_TILES; j++) {
+		for(int j = 0; j < J/NUM_OF_TILES; j++) { // Performs 32 MACs in parallel to calculate the partial sum for each of B's lines
 			#pragma HLS UNROLL
 			psum[j] += a_val * B_streams[j].read();
 		}
@@ -22,7 +23,7 @@ void psumToStream(float psum[J/NUM_OF_TILES], hls::stream<float> C_streams[J/NUM
 	#pragma HLS INLINE off
 	for(int j = 0; j < J/NUM_OF_TILES; j++) {
 		#pragma HLS UNROLL
-		C_streams[j].write(psum[j]);
+		C_streams[j].write(psum[j]); // Pass the partial sums to a stream to be written to the output buffer C
 	}
 }
 
@@ -30,11 +31,11 @@ void matMulTile(hls::stream<float> &A_stream, hls::stream<float> B_streams[J/NUM
 	#pragma HLS INLINE off
 	mat_mul_i:
 	for(int i = 0; i < I/NUM_OF_TILES; i++) {
-		#pragma HLS DATAFLOW
-		float psum[J/NUM_OF_TILES] = {0};
-		#pragma HLS ARRAY_PARTITION variable=psum type=complete dim=1
-		calculatepsum(A_stream, B_streams, psum);
-		psumToStream(psum, C_streams);
+		#pragma HLS DATAFLOW // Concurrent execution of functions
+		float psum[J/NUM_OF_TILES] = {0}; // Initialize the partial sum array to zero for each tile
+		#pragma HLS ARRAY_PARTITION variable=psum type=complete dim=1 // Partition for parallel access to elements
+		calculatepsum(A_stream, B_streams, psum); // Calculate psum
+		psumToStream(psum, C_streams); // Store psum
 	}
 }
 
@@ -43,7 +44,7 @@ void matMulTiles(hls::stream<float> A_streams[NUM_OF_TILES], hls::stream<float> 
 	#pragma HLS DATAFLOW
 	for(int t=0; t<NUM_OF_TILES; t++){
 		#pragma HLS UNROLL
-		matMulTile(A_streams[t], B_streams[t], C_streams[t]);
+		matMulTile(A_streams[t], B_streams[t], C_streams[t]); // Perform MatMul on 1 tile of A and 2 tiles of B, in parallel
 	}
 }
 
